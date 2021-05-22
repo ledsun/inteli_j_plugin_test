84df6ba4fbfec1785b04c5cf80471200bb0aa0c5
GitHub Action <action@github.com>
2021-05-22 14:44:10 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "inteli_j_plugin_test"
--- src/main/kotlin/com/github/ledsun/intelijplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ledsun/intelijplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ledsun.intelijplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ledsun.intelijplugintest.MyBundle
 
 class MyApplicationService {
 
